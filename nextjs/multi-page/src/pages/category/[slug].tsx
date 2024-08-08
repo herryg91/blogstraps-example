4abe741701b9258943a320cbc3c6e5ed4dcc9559
Herry Gunawan <herry@gajikuapp.com>
2024-08-08 15:19:55 +0700

    change name multi page to ssr

---
+++ nextjs/multi-page/src/pages/category/[slug].tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps, NextPage } from "next";
+import MainLayout from "@/components/layouts/main-layout";
+import Script from "next/script";
+
+const CategoryPage: NextPage<{ category_slug: string }> = (param) => {
+  return <>
+    <Script async src="https://blogstraps.com/embed/blogstraps.min.js"></Script>
+    <MainLayout>
+      <div id="bs-blog" data-key={process.env.NEXT_PUBLIC_BLOGSTRAPS_EMBED_KEY} data-category={param.category_slug}></div> 
+    </MainLayout>
+  </>
+}
+
+export default CategoryPage
+
+export const getServerSideProps: GetServerSideProps<{ category_slug: string }> = async (context) => {
+  const category_slug = context.params?.slug?.toString() ?? ""
+  return { props: { category_slug: category_slug } }
+}
---
+++ nextjs/multi-page/src/pages/post/[slug].tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps, NextPage } from "next";
+import MainLayout from "@/components/layouts/main-layout";
+import Script from "next/script";
+
+const PostPage: NextPage<{ post_slug: string }> = (param) => {
+  return <>
+    <Script async src="https://blogstraps.com/embed/blogstraps.min.js"></Script>
+    <MainLayout>
+      <div id="bs-blog" data-key={process.env.NEXT_PUBLIC_BLOGSTRAPS_EMBED_KEY} data-post={param.post_slug}></div> 
+    </MainLayout>
+  </>
+}
+
+export default PostPage
+
+export const getServerSideProps: GetServerSideProps<{ post_slug: string }> = async (context) => {
+  const post_slug = context.params?.slug?.toString() ?? ""
+  return { props: { post_slug: post_slug } }
+}
